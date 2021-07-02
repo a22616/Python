9f2b999aa842cd6607d67d349159780437505aef
a22616 <81513107+a22616@users.noreply.github.com>
2021-07-02 12:30:54 +0530

    Add files via upload

---
+++ variables_datatype.py
@@ -0,0 +1,41 @@
+'''
+7 WHAT IS VARIABLES ?
+ans : Variables are containers for storing data values.
+
+8 WHAT IS DATA TYPE ?
+ans : Variables can store data of different types, and different types can do different things.
+    like integers , floating point ,strings, booleans , none
+
+9 EXAMPLE OF DATA TYPE
+ans : integer a = 5 float b = 8.5 string c = "abc" booleans d = true/false
+'''
+a = 35
+b = 88.8
+c = "anand"
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+'''
+10  WHAT IS OPRETOR ?
+ans : Operators are used to perform operations on variables and values.
+
+11 TELL DIFFERENT OPERATORS IN PYTHON
+ans : Arithmetic operators --> +,-,*,/,%,**,//
+      Assignment opretors --> =,+=,-=,*=,/=,%=,//=,**=,&=,!=,^=,>>=,<<=
+      comparition opretors --> ==,>,!=,<,<=,>=
+      logical opretors --> and ,or ,not
+      identity opretors --> is , is not
+      Membership opretor --> in ,in not
+      Bitwise opretor --> &,!,^,>>,<<,~
+
+'''
+# Example of opreter
+print(a+b)
+print(a==b)
+print(a-b)
+print(d or e)
+print(b % a)
