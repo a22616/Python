03b80ea26e4a8545228af5187317510232c36d48
a22616 <81513107+a22616@users.noreply.github.com>
2021-07-02 12:29:34 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+'''
+1 WHAT IS PROGRAMING ?
+ans : Programming is a collaboration between humans and computers.
+    Put simply, programming is giving a set of instructions to a computer to execute.
+
+2 WHAT IS PROGRAMING Language ?
+ans : A programming language is a computer language
+    that is used by programmers (developers) to communicate with computers.
+    And programming languages are the tools we use to write instructions for computers to follow. 
+
+3 WHAT IS PYTHON ?
+ans : Python is a simple and easy to understand programing language.
+'''
+print("hello world")
+
+'''
+4 WHAT IS MODULES ?
+ans : A module is a file containing cod written by somebody which can be imported and and used
+    in our programe.
+
+5 TYPE OF MODULES
+ans : Built in modules --> pre installed in python
+      External modules --> Need to install usining pip
+
+6 TYPE OF COMMENT
+ans : Single line comment --> written using #
+      Multi line comment --> written using '''
