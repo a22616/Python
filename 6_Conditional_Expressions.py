9e831f2177d90c5a63bbe56fe427b513ba047348
a22616 <81513107+a22616@users.noreply.github.com>
2021-07-04 00:26:41 +0530

    Add files via upload

---
+++ 6_Conditional_Expressions.py
@@ -0,0 +1,21 @@
+'''
+21 WHAT IS CONDITIONAL EXPRESSION ?
+ans : In conditional expression All decisionsndependens on condition.
+    in python programming, We must be able to execute instruction on condition.
+    Opretors are used to evaluate condition inside the if..else statement.
+    IF ELSE AND ELIF IN PYTHON
+    syntax 
+    if(condition1):
+        print()
+    elif(condition2):
+        print()
+    else():
+        print()
+
+'''
+print("example if else statement \n")
+a = 33
+if a > 18:
+    print("a is a bigger value")
+else:
+    print("a is a smaller value")
